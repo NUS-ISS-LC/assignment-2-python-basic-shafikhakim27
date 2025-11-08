0428ac74498606d31d1aeae2b422ab5e0186eba9
Shafik Hakim Bin Abdul Wahid <E1582515@u.nus.edu>
2025-11-08 01:06:07 +0000

    change

--- ex1.py
+++ ex1.py
@@ -1,3 +1,14 @@
 def find(s, n):
-# write your implementation here
+    # Search for n in the list s
+    for i in range(len(s)):
+        if s[i] == n:
+            return i
     return None
+
+# Produce an integer array
+nums = [10, 20, 30, 40, 50]
+
+# Test the function
+print("Array:", nums)
+print("Find 30:", find(nums, 30))   # Output: 2
+print("Find 100:", find(nums, 100)) # Output: None
